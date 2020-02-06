c6dd5ca91d32087d5cff588493ad897918ff82bb
Zoey <air@airdeMacBook-Air.local>
2020-02-05 22:52:56 -0600

    first commit

---
+++ datascience_repo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the project for Data Science class.',
+    author='Zoe-yz',
+    license='MIT',
+)
